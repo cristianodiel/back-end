ddd5506e0f228d954c6f691e03d41654a9b38f72
cristianodiel <cristianodiel@gmail.com>
2022-09-08 18:04:24 -0300

    22-09-08 adicionando arquivos elaborados em sala

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("oi script")
+const vetor = [1,2,3];
+console.log(vetor);
+vetor[10]=true;
+console.log(vetor);
+
+function insert_value(){
+    vetor[vetor.length]=prompt("digite seu valor");
+    console.log(vetor);
+}
+let a = "A";
+
+typeof(a);//no console
+let b = 2;
+typeof(b);//no console
+let c = false;
+typeof(c);// no console
