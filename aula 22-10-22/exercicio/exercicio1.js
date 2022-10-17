2f966739c168b1a04d43e53e307034ec82c1ea3f
cristianodiel <cristianodiel@gmail.com>
2022-10-17 14:30:54 -0300

    aula 22_10_17

---
+++ aula 22-10-22/exercicio/exercicio1.js
@@ -0,0 +1,17 @@
+let resultado = document.getElementById("resultado");
+
+function soma(a,b){
+    resultado.innerText = parseInt(a) + parseInt(b);
+}
+function subtracao(a,b){
+    resultado.innerText = parseInt(a) - parseInt(b);
+}
+function multiplicacao(a,b){
+    resultado.innerText = parseInt(a) * parseInt(b);
+}
+function divisao(a,b){
+    resultado.innerText = parseInt(a) / parseInt(b);
+}
+
+
+
