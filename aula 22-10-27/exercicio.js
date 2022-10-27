060719a72e806407c265e8b89597ee0959308ce0
cristiano diel <cristianodiel@gmail.com>
2022-10-27 18:17:35 -0300

    aula 221027

---
+++ aula 22-10-27/exercicio.js
@@ -0,0 +1,8 @@
+const mae = document.getElementById('mae');
+const elemento = document.createElement('h1');
+const button = document.createElement('button');
+button.innerText = "botao";
+ 
+
+mae.appendChild(elemento);
+body.appendChild(button);
---
+++ aula 22-10-27/script.js
@@ -0,0 +1,47 @@
+// const inputs =document.getElementsByTagName('input');
+
+// console.log(inputs);
+
+// const classes = document.getElementsByClassName('primeiro');
+// console.log(classes);
+
+// const primeiraClasse = document.querySelector('.primeiro');
+
+// console.log(primeiraClasse);
+
+// const elementoComId = document.querySelector('#id1');
+// console.log(elementoComId);
+/*document.querySelectorAll(seletor)*/
+/*serve para selecionar todos (e nao apenas o primeiro) */
+
+/*exemplo a partir daqui*/
+
+// const cpfs = document.querySelectorAll('li');
+// console.log(cpfs);
+
+// const filtrar = document.getElementById('filtrar');
+
+// filtrar.addEventListener('click', function(){
+//     cpfs.forEach(function(item){
+//         if(item.innerText.length !== 11){
+//             item.style.backgroundColor = 'red';
+//         }
+//     })
+// })
+
+// /*exemplo2*/
+// const terceiro = document.getElementById('terceiro');
+// const segundo = document.getElementById('segundo');
+
+/*exemplo3*/
+// const elemento = document.createElement('button');
+// console.log(elemento);
+
+// elemento.id = "idElemento";
+// elemento.innerText = "Clique no elemento";
+// elemento.addEventListener('click', function(){
+    
+// })
+// console.log("clicou no elemento");
+
+// const pai = document.getElementById('pai');
